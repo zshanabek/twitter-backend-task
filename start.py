94292c351d1b75898f248e585192dc17d157416a
Zhunissali Shanabek <zshanabek@gmail.com>
2018-09-25 18:04:58 +0300

    [Add] - get tweets zshanabek

--- start.py
+++ start.py
@@ -1,10 +1,26 @@
-import twint
+from twitter_scraper import get_tweets
+import pprint
+from flask import Flask, jsonify
 
-# Configure
-c = twint.Config()
-c.Username = "now"
-c.Search = "pineapple"
-c.Format = "Tweet id: {id} | Tweet: {tweet}"
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-# Run
-twint.run.Search(c)
+tweets = []
+
+
+for tweet in get_tweets('zshanabek', pages=1):
+	tweets.append({
+		'date': tweet['time'],
+		'likes': tweet['likes'],
+		'replies': tweet['replies'],
+		'retweets': tweet['retweets'],
+		'text': tweet['text']
+	})
+
+
+@app.route('/test/api/v1.0/tweets', methods=['GET'])
+def get_tasks():
+    return jsonify({'tweets': tweets})
+
+if __name__ == '__main__':
+    app.run(debug=True)
